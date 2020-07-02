582ccc40e5f9bbcac7e9a1f8f7ebb6afe0be1438
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 14:38:31 +0700

    add

---
+++ Module_2/APC/tinhLaiSuat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tinhLaiSuat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money;
+            int month;
+            double intersetRate;
+
+            Console.WriteLine("Nhập số tiền: ");
+            money = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhập số tháng gửi: ");
+            month = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhập % lãi suất: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+
+            double totalInterset = 0;
+            for (int i = 0; i < month; i++)
+            {
+                totalInterset = money * (intersetRate / 100) / 12 * 3;
+            }
+            Console.WriteLine("Total of interset: " + totalInterset);
+
+        }
+    }
+}
