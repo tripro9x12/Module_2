f0849e0277c661ab34899057a23d5f498d81a163
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 15:20:19 +0700

    bt pt bậc 1

---
+++ Module_2/APC/PT_Bac1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PT_Bac1 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Nhập a:");
+            double a = Double.Parse (Console.ReadLine ());
+            Console.WriteLine ("Nhập b:");
+            double b = Double.Parse (Console.ReadLine ());
+            if (a == 0) {
+                if (b == 0) {
+                    Console.Write ("PT vô nghiệm");
+                } else {
+                    Console.Write ("PT vô số nghiệm");
+                }
+            } else {
+                double x;
+                x = -b / a;
+                Console.Write ($"PT có nghiệm x = {x} ");
+            }
+        }
+    }
+}
