0777c7323a27f63fa9c83493dd6433539f2fc094
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 15:41:18 +0700

    add hello

---
+++ Module_2/APC/showHello/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace showHello {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Nhập tên người cần chào: ");
+            string name = Console.ReadLine ();
+            Console.WriteLine ($"Hello {name} ");
+        }
+    }
+}
