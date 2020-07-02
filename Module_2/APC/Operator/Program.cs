4ce611c661acb152dc9cd52326bad10bf66cf7b9
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 14:03:53 +0700

    add bt area_rectangle

---
+++ Module_2/APC/Operator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+
+            Console.Write("Nhập chiều rộng: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Nhập chiều dài: ");
+            height = float.Parse(Console.ReadLine());
+
+            float area = width * height;
+            Console.WriteLine("diện tích là:: " + area);
+
+        }
+    }
+}
