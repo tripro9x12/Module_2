4bbb3d358909c1eec931ec97546539c27047d887
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 15:08:59 +0700

    debug

---
+++ Module_2/APC/debug/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace debug {
+    class Program {
+        static void Main (string[] args) {
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine ("Index = " + i);
+            }
+        }
+    }
+}
