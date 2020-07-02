331b73d682591c533f3346312bb1458069d30088
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-02 14:47:24 +0700

    add

---
+++ Module_2/APC/UCLN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UCLN {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Enter a: ");
+            int a = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("Enter b: ");
+            int b = Convert.ToInt32 (Console.ReadLine ());
+            a = Math.Abs (a);
+            b = Math.Abs (b);
+            if (a == 0 || b == 0) {
+                Console.WriteLine ("No greatest common factor");
+            }
+            while (a != b) {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            Console.WriteLine ("Greatest common factor: " + a);
+
+        }
+
+    }
+}
