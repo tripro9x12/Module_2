f61e36042fd08725cc3d2d161c4135b5cd61b44f
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-05 13:00:35 +0700

    dem SNT be hon 100

---
+++ Module_2/APC/CountSNT_BeHon100/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CountSNT_BeHon100 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Nhập SNT cần log ra: ");
+            int count = 0;
+            int n = 0;
+            while (n < 100) {
+                if (checkSNT (n) == true) {
+                    Console.WriteLine ($"Lần {count}: {n} ");
+                    count++;
+                }
+                n++;
+            }
+        }
+        static bool checkSNT (int num) {
+            if (num < 2) {
+                return false;
+            } else if (num == 2) {
+                return true;
+            } else {
+                int dem = 0;
+                for (int i = 1; i <= num; i++) {
+                    if (num % i == 0) {
+                        dem++;
+                    }
+                }
+                if (dem == 2) {
+                    return true;
+                } else return false;
+            }
+        }
+
+    }
+}
