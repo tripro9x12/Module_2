9f8f07c2571dbdee104ca91fb6e85d48399585c7
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-05 12:30:12 +0700

    add bt covert USD

---
+++ Module_2/APC/convertMoney/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace convertMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rate = 23000;
+            Console.WriteLine("Nhập số tiền USD muốn đổi: ");
+            int USD = Int32.Parse(Console.ReadLine());
+            int VND;
+            VND = USD * rate;
+            Console.Write("số tiền sau khi convert là: {0} ", VND);
+        }
+    }
+}
