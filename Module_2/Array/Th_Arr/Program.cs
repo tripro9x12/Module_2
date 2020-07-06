b4c5ccf2ffb3ca3599142a93ab8b38bd2a58bb35
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-06 13:46:55 +0700

    update

---
+++ Module_2/Array/Th_Arr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Th_Arr {
+    class Program {
+        static void Main (string[] args) {
+            int[] number = new int[5];
+            number[0] = 2;
+            number[1] = 5;
+            number[2] = 9;
+            number[3] = 6;
+            number[4] = 7;
+            Console.WriteLine ($"{number[0]}, {number[2]}, {number[3]} ");
+            int total = 0;
+            for (int i = 0; i < number.Length; i++) {
+                total = total + number[i];
+            }
+            Console.WriteLine (total.ToString ());
+        }
+    }
+}
