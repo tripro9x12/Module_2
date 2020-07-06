d4f25d11cdb5332b29e51f2024e0353ab98ec0ef
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-06 11:55:34 +0700

    add

---
+++ Module_2/Array/Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TH_Array TH_1 = new TH_Array();
+            TH_1.PrintArray();
+
+            Console.ReadKey();
+        }
+    }
+}
