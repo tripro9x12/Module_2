894fb0a834cb3ef69650a5499fa62a7f122286e4
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-07 14:36:37 +0700

    update

--- Module_2/AcsessModifile/ProductManagement/Product.cs
+++ Module_2/AcsessModifile/ProductManagement/Product.cs
@@ -6,5 +6,15 @@ namespace ProductManagement
 {
     class Product
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Menufactory { get; set; }
+
+        public string ShowProduct()
+        {
+            return 
+        }
     }
 }
--- Module_2/AcsessModifile/ProductManagement/Program.cs
+++ Module_2/AcsessModifile/ProductManagement/Program.cs
@@ -6,7 +6,8 @@ namespace ProductManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
