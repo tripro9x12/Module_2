8cfadc6dbf46238277c3af539d08b584b1075361
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-08 23:32:20 +0700

    add bt chèn số vào mảng

---
+++ Module_2/Array/Add_Index/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Add_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhập n:");
+            n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 0;
+            }
+            Console.WriteLine("Nhập X số muốn chèn: ");
+            int X = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập index là vị trí muốn chèn");
+            int index = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (index <= 1 && index >= (arr.Length - 1))
+                {
+                    Console.WriteLine("không chèn được phần tử vào mảng");
+                }
+                else
+                {
+                    if (i == index)
+                    {
+                        for (int j = arr.Length - 1; j > index; j--)
+                        {
+                            arr[j] = arr[j - 1];
+                        }
+                        arr[index] = X;
+
+                    }
+                }
+            }
+            Console.WriteLine("Mảng sau khi chèn là: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
