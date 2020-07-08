7ed00622109caedaeb05b275b3a0383d480beeaa
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-09 00:12:07 +0700

    add bt delete index

---
+++ Module_2/Array/DeleteIndex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DeleteIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhập n: ");
+            n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]: ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Nhập phần tử X muốn xóa: ");
+            int X = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == X)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            arr[arr.Length - 1] = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
