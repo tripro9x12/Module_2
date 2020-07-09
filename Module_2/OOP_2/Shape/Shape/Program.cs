72cec0e9899af91bfdee27fce05bcfd403904a7f
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-09 11:23:12 +0700

    add th_shape

---
+++ Module_2/OOP_2/Shape/Shape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5);
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5, "indigo", false);
+            Console.WriteLine(circle);
+
+
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.3, 5.8);
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+            Console.WriteLine(rectangle);
+
+        }
+    }
+}
