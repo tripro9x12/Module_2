641343dbbf0434c54fa564cf27e73c9ddced983e
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-09 15:10:14 +0700

    add Point update

---
+++ Module_2/OOP_2/Point/Point/MoveablePoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Point
+{
+    public class MoveablePoint : Point2D
+    {
+        public float xSpeed = 0.0f;
+        public float ySpeed = 0.0f;
+        public MoveablePoint() { }
+        public MoveablePoint(float xSpeed, float ySpeed, float x, float y ) : base(x,y)
+        {
+            this.xSpeed = xSpeed;
+            this.ySpeed = ySpeed;
+        }
+        public float XSpeed
+        {
+            get => this.xSpeed;
+            set => this.xSpeed = value;
+        }
+        public float YSpeed
+        {
+            get => this.ySpeed;
+            set => this.ySpeed = value;
+        }
+
+        public float[] GetSpeed()
+        {
+            arr[0] = this.x;
+            arr[1] = this.y;
+            return arr;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public MoveablePoint Move()
+        {
+            this.x += xSpeed;
+            this.y += ySpeed;
+            //a = this.x;
+            //b = this.y;
+            return this;
+
+        }
+
+    }
+}
--- Module_2/OOP_2/Point/Point/Program.cs
+++ Module_2/OOP_2/Point/Point/Program.cs
@@ -14,6 +14,8 @@ namespace Point
                 Console.WriteLine(point2d.getXY()[i]);
             }
 
+
+
             Point3D point3d = new Point3D();
             point3d.setXZY(4.5f, 5f, 6f);
             Console.WriteLine("Arr point2");
@@ -22,6 +24,22 @@ namespace Point
                 Console.WriteLine(point3d.getXYZ()[i]);
             }
 
+
+            MoveablePoint moveablePoint = new MoveablePoint(0f,0f,4f,6f);
+        
+            moveablePoint.xSpeed = 5f;
+            moveablePoint.YSpeed = 4f;
+            Console.WriteLine("arr moveablePoint");
+            for(int i = 0; i<moveablePoint.GetSpeed().Length; i++)
+            {
+                Console.Write("index: "+ i+" = "+moveablePoint.GetSpeed()[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine(moveablePoint.ToString());
+
+            Console.WriteLine($"xSpeed = {moveablePoint.xSpeed},ySpeed = {moveablePoint.ySpeed}");
+
+            Console.WriteLine(moveablePoint.Move());
 
             Console.ReadKey();
         }
