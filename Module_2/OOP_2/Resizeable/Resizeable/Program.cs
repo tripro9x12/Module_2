182c271b24089c91fb138b541ca3620cf8ce0c60
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-09 22:16:04 +0700

    [Bài tập ] Triển khai interface 'Resizeable' cho các lớp hình học

---
+++ Module_2/OOP_2/Resizeable/Resizeable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Resizeable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resizeable[] resizeables = new Resizeable[3];
+            resizeables[0] = new Circle();
+            resizeables[1] = new Rectangle();
+            resizeables[2] = new Square();
+            Console.WriteLine("Trước khi resize");
+            for (int i = 0; i < resizeables.Length; i++)
+            {
+                Console.WriteLine(resizeables[i].Size(5));
+            }
+            Console.WriteLine("Sau khi resize");
+            for (int i = 0; i < resizeables.Length; i++)
+            {
+                //Console.WriteLine(resizeables[i].Resize(5));
+                resizeables[i].Resize(5);
+            }
+        }
+    }
+}
---
+++ Module_2/OOP_2/Resizeable/Resizeable/Resizeable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resizeable
+{
+    public interface Resizeable
+    {
+        public void Resize(double parcent);
+        public double Size(double parcent);
+    }
+
+
+    public class Circle : Resizeable
+    {
+        public void Resize(double parcent)
+        {
+            parcent = parcent * new Random().Next(1, 100);
+            Console.WriteLine($"kích thước Circle tăng lên: {parcent}");
+        }
+        public double Size(double parcent)
+        {
+            return parcent;
+        }
+    }
+    public class Rectangle : Resizeable
+    {
+        public void Resize(double parcent)
+        {
+            parcent = parcent * new Random().Next(1, 100);
+            Console.WriteLine($"kích thước Rectangle tăng lên: {parcent}");
+        }
+        public double Size(double parcent)
+        {
+            return parcent;
+        }
+    }
+    public class Square : Resizeable
+    {
+        public void Resize(double parcent)
+        {
+            parcent = parcent * new Random().Next(1, 100);
+            Console.WriteLine($"kích thước Squale tăng lên: {parcent}");
+        }
+        public double Size(double parcent)
+        {
+            return parcent;
+        }
+    }
+}
