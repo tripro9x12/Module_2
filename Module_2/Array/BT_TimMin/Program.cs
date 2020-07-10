c810a985e529ba15b07788b5036e312f08b1a9f5
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-10 10:14:30 +0700

    Tìm giá trị nhỏ nhất trong mảng

---
+++ Module_2/Array/BT_TimMin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BT_TimMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số lượng phần tử của arr: ");
+            int size = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}] = ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            int min = arr[0];
+            int index = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine($"index = {index}, min = {min}");
+        }
+    }
+}
