7e452b9d90e246f8a45430b49b7ebcc5affc5502
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-10 10:43:34 +0700

    [*Bài tập] Đếm số lần xuất hiện của ký tự trong chuỗi

---
+++ Module_2/Array/CountCharInString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CountCharInString {
+    class Program {
+        static void Main (string[] args) {
+            int count = 0;
+            string str = "hoc lap trinh csharp";
+            Console.Write ("Nhập ký tự muốn đếm: ");
+            char c = Char.Parse (Console.ReadLine ());
+
+            for (int i = 0; i < str.Length; i++) {
+                if (str[i] == c) {
+                    count++;
+                }
+            }
+            Console.WriteLine ($"count = {count}");
+        }
+    }
+}
