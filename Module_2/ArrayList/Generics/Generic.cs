4a01f985b3fbfea3bb574e834f0c3fa8ccc93e86
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-20 10:43:19 +0700

    [Bài Tập] Khởi tạo hàm sử dụng generics

---
+++ Module_2/ArrayList/Generics/Generic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Generic<T>
+    {
+        public bool Equal(T value_1, T value_2)
+        {
+            return value_1.Equals(value_2);
+        }
+    }
+}
---
+++ Module_2/ArrayList/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> generic_1 = new Generic<int>();
+            Console.WriteLine(generic_1.Equal(3, 4));
+            Console.WriteLine(generic_1.Equal(4, 4));
+
+            Generic<string> generic_2 = new Generic<string>();
+            Console.WriteLine(generic_2.Equal("dog", "dog"));
+            Console.WriteLine(generic_2.Equal("cat", "dog"));
+        }
+    }
+}
