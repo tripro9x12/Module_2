576078b3c067d8eaaeb6e978bdd5ca431e450444
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-20 10:36:18 +0700

    [Bài Tập] Thao tác với List. sử dụng method Add, Remove, IndexOf, Sort

---
+++ Module_2/ArrayList/List/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        public static List<string> myList = new List<string>();
+        static void Main(string[] args)
+        {
+            myList.Add("lion");
+            myList.Add("cat");
+            myList.Add("dog");
+            myList.Add("elephant");
+            Show();
+            DeleteDog();
+            Console.WriteLine("remove dog:");
+            Show();
+            
+            myList.Sort();
+            Console.WriteLine("sort: ");
+            Show();
+            ShowIndexCat();
+        }
+        public static void Show()
+        {
+            foreach(var list in myList)
+            {
+                Console.WriteLine(list);
+            }
+        }
+        public static void DeleteDog()
+        {
+            for(int i = 0; i<myList.Count;i++)
+            {
+                if(myList[i] == "dog")
+                {
+                    myList.Remove("dog");
+                }
+            }
+        }
+        public static void ShowIndexCat()
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] == "cat")
+                {
+                    Console.WriteLine($"index cat = {i}");
+                }
+            }
+        }
+    }
+}
