11ab91910ff914075d8a18b07636d9dd08513317
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-20 19:57:10 +0700

    update

--- Module_2/Array/BT_Array/Array_SortAndSearch.cs
+++ Module_2/Array/BT_Array/Array_SortAndSearch.cs
@@ -85,14 +85,17 @@ namespace BT_Array
         }
         public static bool IsSymmetryArray(int[] arr)
         {
+            
             for(int i = 0; i < arr.Length / 2; i++)
             {
-                if(arr[i] == arr[arr.Length -1 - i])
+                if(arr[i] != arr[arr.Length -1 - i])
                 {
-                    return true;
+                    return false;
                 }
+                
             }
-            return false;
+            return true;
+           
         }
         public static void SortArray(ref int[] arr,ref bool check)
         {
