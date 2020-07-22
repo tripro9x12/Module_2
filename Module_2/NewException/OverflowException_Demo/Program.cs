7b74c6dd595b29c5a248f4a15c0702a033fe05b2
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-22 16:39:10 +0700

    [Bài tập] Sử dụng lớp OverflowException

---
+++ Module_2/NewException/OverflowException_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OverflowException_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter num 1: ");
+                int number_1 = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter num 2: ");
+                int number_2 = Int32.Parse(Console.ReadLine());
+
+                int multi = number_1 * number_2;
+                Console.WriteLine(multi);
+            }
+            catch (OverflowException e)
+            {
+
+                Console.WriteLine($"{e.GetType().Name}, {e.Message}");
+            }
+        }
+    }
+}
