06084feac9f91471558880665a326181ecdcb2ac
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-22 11:48:21 +0700

    [Thực hành] Sử dụng lớp FormatException

---
+++ Module_2/NewException/FormatException_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FormatException_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //float price = 169.32f;
+                //Console.WriteLine("The cost is {0}.", price);
+                decimal price = 169.32m;
+                Console.WriteLine("The cost is {0:Q2}.", price);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
