1c106ad308166b01abeba1d658026af1a1c03031
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-22 11:40:11 +0700

    [Thực hành] Sử dụng lớp FileNotFoundException

---
+++ Module_2/NewException/FileNotFoundException_Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace FileNotFoundException_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Read in nonexistent file.
+                using (StreamReader reader = new StreamReader("not-there.txt"))
+                {
+                    reader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                // Write error.
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
