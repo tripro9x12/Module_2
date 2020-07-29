1fe9702dd158a731602ef2b933e942ec5fc3db5b
tripro9x12@gmail.com <tripro9x12@gmail.com>
2020-07-29 15:09:43 +0700

    [Bài tập] Hiển thị số chẵn/lẻ

--- Module_2/Threads/ThreadDemo/Program.cs
+++ Module_2/Threads/ThreadDemo/Program.cs
@@ -7,12 +7,12 @@ namespace ThreadDemo
     {
         static void Main(string[] args)
         {
-            NumberGenerator num_1 = new NumberGenerator();
+            NumberGenerator num_1 = new NumberGenerator("Thread 1");
             Thread thread_1 = new Thread(num_1.Run);
             thread_1.Priority = ThreadPriority.Highest;
             thread_1.Start();
 
-            NumberGenerator num_2 = new NumberGenerator();
+            NumberGenerator num_2 = new NumberGenerator("Thread 2");
             Thread thread_2 = new Thread(num_2.Run);
             thread_2.Priority = ThreadPriority.Lowest;
             thread_2.Start();
@@ -23,11 +23,16 @@ namespace ThreadDemo
     }
     public class NumberGenerator
     {
+        public string Name { get; set; }
+        public NumberGenerator(string name)
+        {
+            this.Name = name;
+        }
         public void Run()
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine($"{this.Name}: {i}");
                 Thread.Sleep(500);
             }
         }
---
+++ Module_2/Threads/ThreadDemo/TestThread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    public class TestThread
+    {
+        public static void Main()
+        {
+            OddThread odd = new OddThread();
+            Thread thread_odd = new Thread(odd.Run);
+            thread_odd.Start();
+            thread_odd.Join();
+            EvenThread even = new EvenThread();
+            Thread thread_even = new Thread(even.Run);
+            thread_even.Start();
+        }
+    }
+    public class OddThread
+    {
+        public void Run()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+    public class EvenThread
+    {
+        public void Run()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(15);
+                }
+            }
+        }
+    }
+}
